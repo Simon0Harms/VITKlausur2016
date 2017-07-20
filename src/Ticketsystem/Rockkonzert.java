421d733264e50347c275b7067b1de38a3b0bf8e1
JokerGermany <sp4m.an.joker@gmail.com>
2017-07-20 18:12:23 +0200

    Konstruktor von Rockkonzert verbessert

--- src/Ticketsystem/Rockkonzert.java
+++ src/Ticketsystem/Rockkonzert.java
@@ -7,19 +7,16 @@ public class Rockkonzert extends Veranstaltung
 	private Band hauptact;
 	public Rockkonzert(String name, LocalDate datum, String ort, int maximaleAnzahlVerfuegbarerTickets )
 	{
-		this(name,datum,ort,maximaleAnzahlVerfuegbarerTickets,null);
-	}
-	public Rockkonzert(String name, LocalDate datum, String ort, int maximaleAnzahlVerfuegbarerTickets, Band band )
-	{
 		this.setOrt(ort);
 		this.setDatum(datum);
 		this.setName(name);
 		this.setMaximaleAnzahlTickets(maximaleAnzahlVerfuegbarerTickets);
-		if (band != null)
-		{	
-			this.hauptact=band;
-			this.hauptact.fuegeKonzertHinzu(this);
-		}	
+	}
+	public Rockkonzert(String name, LocalDate datum, String ort, int maximaleAnzahlVerfuegbarerTickets, Band band )
+	{
+		this(name,datum,ort,maximaleAnzahlVerfuegbarerTickets);
+		this.hauptact=band;
+		this.hauptact.fuegeKonzertHinzu(this);	
 	}
 	
 	public String gibInfo()
